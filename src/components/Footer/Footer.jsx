ebfe799874fbfdbb1aaffda02c5a89ff559fda4f
Lomorng Thoeung <thoeunglomorng1@gmail.com>
2026-02-01 10:58:05 +0700

    third commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
-          <img src={assets.logo} alt="" />
+          <img src={assets.logo_food} alt="" />
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
             similique vitae doloremque, tempore, sit expedita obcaecati
